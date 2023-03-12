e5aa0f7c35d647ff256072ba28d7a0c8e92d7524
JuampaVLB <juampablovillalba25@gmail.com>
2023-03-12 01:36:13 -0300

    add animates with animate.css

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "../styles/Socials.module.css";
 
+import 'animate.css';
+
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
@@ -9,19 +11,19 @@ import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 function Socials() {
   return (
     <div className={styles.box}>
-      <a href="https://github.com/JuampaVLB" target="_BLANK">
+      <a href="https://github.com/JuampaVLB" target="_BLANK" className="animate__animated animate__zoomInLeft">
         <GitHubIcon />
       </a>
 
-      <a href="https://www.linkedin.com/in/juanpablovillalba/" target="_BLANK">
+      <a href="https://www.linkedin.com/in/juanpablovillalba/" target="_BLANK" className="animate__animated animate__zoomInLeft">
         <LinkedInIcon />
       </a>
 
-      <a href="mailto:contacto.juanpablovillalba@gmail.com" target="_BLANK">
+      <a href="mailto:contacto.juanpablovillalba@gmail.com" target="_BLANK" className="animate__animated animate__zoomInLeft">
         <EmailIcon />
       </a>
 
-      <a href="#top" className={styles.arrow}>
+      <a href="#top" className={`${styles.arrow} animate__animated animate__slideInRight`}>
         <ArrowUpwardIcon />
       </a>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import styles from '../styles/Home.module.css'
 
+import 'animate.css';
+
 function Home() {
   return (
     <div className={styles.container} id='home'>
-      <div className={styles.presentation}>
+      <div className={`${styles.presentation} animate__animated animate__zoomIn`}>
         <p>Hola, mi nombre es</p>
         <h1>Juan Pablo Villalba</h1>
         <h2>Full Stack Developer</h2>
         <a href='/docs/Juan Pablo Villalba.pdf' target="_BLANK">Descargar CV</a>
       </div>
       <div className={styles.image}>
-      <img src="/img/profile.png" alt="Juan Pablo Villalba" />
+      <img src="/img/profile.png" alt="Juan Pablo Villalba" className='animate__animated animate__zoomIn' />
       </div>
       
     </div>
