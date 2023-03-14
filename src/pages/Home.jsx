70cfb38c5595df075fee60f28b356dc7b90ad4d3
JuampaVLB <juampablovillalba25@gmail.com>
2023-03-13 21:02:31 -0300

    update section home change background in darkmode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,40 @@
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import styles from "../styles/Home.module.css";
 
-import 'animate.css';
+import { useColorMode } from "@chakra-ui/color-mode";
+
+import "animate.css";
 
 function Home() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <div className={styles.container} id='home'>
-      <div className={`${styles.presentation} animate__animated animate__zoomIn`}>
+    <div
+      className={`${styles.container} ${
+        colorMode === "light" ? "" : styles.active
+      }`}
+      id="home"
+    >
+      <div
+        className={`${styles.presentation} animate__animated animate__zoomIn`}
+      >
         <p>Hola, mi nombre es</p>
         <h1>Juan Pablo Villalba</h1>
         <h2>Full Stack Developer</h2>
-        <a href='/docs/Juan Pablo Villalba.pdf' target="_BLANK">Descargar CV</a>
+        <a href="/docs/Juan Pablo Villalba.pdf" target="_BLANK">
+          Descargar CV
+        </a>
       </div>
       <div className={styles.image}>
-      <img src="/img/profile.png" alt="Juan Pablo Villalba" className='animate__animated animate__zoomIn' />
+        <img
+          src="/img/profile.png"
+          alt="Juan Pablo Villalba"
+          className="animate__animated animate__zoomIn"
+        />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
